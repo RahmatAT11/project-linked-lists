af60f1c9b8f4f802d51290b3a7a33e4bc600d5c6
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2024-09-12 15:58:03 +0700

    Finish the project

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 // test linked list
-module.exports = require('./linked-list');
+const linkedListModule = require("./linked-list");
+const linkedList = linkedListModule.createLinkedList();
+
+for (let i = 0; i < 10; i++) {
+  linkedList.append(i);
+}
+
+console.log(linkedList.toString());
