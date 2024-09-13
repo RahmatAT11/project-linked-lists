20d421f453e5f9554ce9c22bd281a7eaf940879a
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2024-09-13 13:05:02 +0700

    Update index.js and package.json

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 // test linked list
 const linkedListModule = require("./linked-list");
-const linkedList = linkedListModule.createLinkedList();
-
-for (let i = 0; i < 10; i++) {
-  linkedList.append(i);
-}
-
-console.log(linkedList.toString());
+module.exports = linkedListModule;
