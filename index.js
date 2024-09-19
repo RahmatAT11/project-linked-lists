eb657e1752c9ca40e2b3a33d87c7dc9a5d56ce78
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2024-09-19 12:35:41 +0700

    Remove test code in index

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
 const linkedListModule = require("./src/linked-list");
-const test = linkedListModule.createLinkedList();
-test.append("acdc");
-console.log(test.removeAt(0));
-console.log(test.getHead())
 module.exports = linkedListModule;
